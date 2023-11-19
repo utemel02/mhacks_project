01d4d8b4a6700df0d808b21ffdc3686075ea4021
Jacob <jakesisawesome1@gmail.com>
2023-11-19 00:32:16 -0500

    now can click tiles

--- lib/ble_controller.dart
+++ lib/ble_controller.dart
@@ -6,19 +6,60 @@ class BleController extends GetxController{
 
   FlutterBlue flutterBlue = FlutterBlue.instance;
 
-  Future scanDevices() async{
+  // Future scanDevices() async{
+  //   var blePermission = await Permission.bluetoothScan.status;
+  //   if(blePermission.isDenied){
+  //     if(await Permission.bluetoothScan.request().isGranted){
+  //       if(await Permission.bluetoothConnect.request().isGranted){
+  //         flutterBlue.startScan(timeout: const Duration(seconds: 10));
+  //         flutterBlue.stopScan();
+  //       }
+  //     }
+  //   }else{
+  //     flutterBlue.startScan(timeout: const Duration(seconds: 10));
+  //
+  //     flutterBlue.stopScan();
+  //
+  //   }
+  //
+  //   // for (var i = 0; i < await scanResults.length; i++) {
+  //   //   print("THIS HERE: " + scanResults)
+  //   // }
+  // }
+  List<ScanResult> devicesList = [];
+  Future scanDevices() async {
     var blePermission = await Permission.bluetoothScan.status;
     if(blePermission.isDenied){
       if(await Permission.bluetoothScan.request().isGranted){
         if(await Permission.bluetoothConnect.request().isGranted){
+          scanResults.listen((List<ScanResult> results) {
+            for (ScanResult result in results) {
+              if (!devicesList.any((device) => device.device.id == result.device.id)) {
+                devicesList.add(result);
+              }
+            }
+            devicesList.sort((a, b) => a.device.id.toString().compareTo(b.device.id.toString()));
+            for (ScanResult result in devicesList) {
+              print('Device Id: ${result.device.id}');
+            }
+          });
           flutterBlue.startScan(timeout: const Duration(seconds: 10));
-
           flutterBlue.stopScan();
         }
       }
-    }else{
+    } else {
+      scanResults.listen((List<ScanResult> results) {
+        for (ScanResult result in results) {
+          if (!devicesList.any((device) => device.device.id == result.device.id)) {
+            devicesList.add(result);
+          }
+        }
+        devicesList.sort((a, b) => a.device.id.toString().compareTo(b.device.id.toString()));
+        for (ScanResult result in devicesList) {
+          print('Device Id: ${result.device.id}');
+        }
+      });
       flutterBlue.startScan(timeout: const Duration(seconds: 10));
-
       flutterBlue.stopScan();
     }
   }
--- lib/main.dart
+++ lib/main.dart
@@ -55,20 +55,34 @@ class _MyHomePageState extends State<MyHomePage> {
                     stream: controller.scanResults,
                     builder: (context, snapshot) {
                       if (snapshot.hasData) {
-                        return ListView.builder(
+                        return Expanded(
+                            child: ListView.builder(
                             shrinkWrap: true,
-                            itemCount: snapshot.data!.length,
+                            // itemCount: snapshot.data!.length,
+                                itemCount: controller.devicesList!.length,
                             itemBuilder: (context, index) {
-                              final data = snapshot.data![index];
-                              return Card(
-                                elevation: 2,
-                                child: ListTile(
-                                  title: Text(data.device.name),
-                                  subtitle: Text(data.device.id.id),
-                                  trailing: Text("${(pow(10, ((-56 - data.rssi) / (10*2))) * 3.2808).toStringAsFixed(2)} ft"),
-                                ),
+                              // final data = snapshot.data![index];
+                              final data = controller.devicesList![index];
+                              return GestureDetector(
+                                  onTap:() => print("Gottem:${Text(data.device.id.id)}"),
+                                  child: Card(
+                                    elevation: 2,
+                                    child: ListTile(
+                                      title: Text(data.device.name),
+                                      subtitle: Text(data.device.id.id),
+                                      trailing: Text("${(pow(10, ((-56 - data.rssi) / (10*2))) * 3.2808).toStringAsFixed(2)} ft"),
+                                    ),
+                                  ),
                               );
-                            });
+                              //   Card(
+                              //   elevation: 2,
+                              //   child: ListTile(
+                              //     title: Text(data.device.name),
+                              //     subtitle: Text(data.device.id.id),
+                              //     trailing: Text("${(pow(10, ((-56 - data.rssi) / (10*2))) * 3.2808).toStringAsFixed(2)} ft"),
+                              //   ),
+                              // );
+                            }));
                       }else{
                         return const Center(child: Text("No Device Found"),);
                       }
